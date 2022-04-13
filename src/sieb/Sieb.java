ea4af5c02f2c7df3f9971c21a01024c8c644cf01
FGBxRamel <kretschmer.raziel@gmail.com>
2022-04-13 23:43:10 +0200

    Now fast

--- src/sieb/Sieb.java
+++ src/sieb/Sieb.java
@@ -1,22 +1,31 @@
 package sieb;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Sieb {
 
 	public static void main(String[] args) {
-		int e = 17;
+		Scanner userIn = new Scanner(System.in);
+		System.out.println("Enter Max-Number");
+		int e = userIn.nextInt();
+		userIn.close();
+		System.out.println("Result:");
 		if (e < 2) {
 			System.out.println("Can't take number below 2!");
 			return;
 		}
-		for (int i = 2; i <= e; i++) {
-			boolean prime = true;
-			for (int j = 2; j < i; j++) {
-				if (i % j == 0) {
-					prime = false;
-					break;
+		boolean[] status = new boolean[e];
+		Arrays.fill(status, true);
+		for (int i = 2; i < Math.sqrt(e); i++) {
+			if (status[i]) {
+				for (int j = i + i; j < e; j += i) {
+					status[j] = false;
 				}
 			}
-			if (prime) {
+		}
+		for (int i = 0; i < status.length; i++) {
+			if (status[i]) {
 				System.out.println(i);
 			}
 		}
