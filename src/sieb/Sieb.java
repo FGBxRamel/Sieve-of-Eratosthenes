8aa38c8f360d2d267a0b462b44413511d122b23d
FGBxRamel <kretschmer.raziel@gmail.com>
2022-06-10 02:23:46 +0200

    Add time calculation

--- src/sieb/Sieb.java
+++ src/sieb/Sieb.java
@@ -17,6 +17,7 @@ public class Sieb {
 		}
 		boolean[] status = new boolean[e];
 		Arrays.fill(status, true);
+		long startTime = System.nanoTime();
 		for (int i = 2; i < Math.sqrt(e); i++) {
 			if (status[i]) {
 				for (int j = i + i; j < e; j += i) {
@@ -24,11 +25,15 @@ public class Sieb {
 				}
 			}
 		}
+		long endTime = System.nanoTime();
+		long timeElapsed = endTime - startTime;
 		for (int i = 0; i < status.length; i++) {
 			if (status[i]) {
 				System.out.println(i);
 			}
 		}
+		float timeMS = (float) (timeElapsed/1000000.0);
+		System.out.print("The calculation took (ns):\n" + timeElapsed + "\nms:" + timeMS);
 	}
 
 }
